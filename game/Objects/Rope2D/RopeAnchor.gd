7e2b951651e078da413c636d9cbc8c620995a92c
Vitalii Shmorhun <3dvits@gmail.com>
2023-08-19 15:41:58 +0300

    Some changes to the rope lenght logic

--- game/Objects/Rope2D/RopeAnchor.gd
+++ game/Objects/Rope2D/RopeAnchor.gd
@@ -4,51 +4,53 @@ var rope_seg_preload = preload("res://Objects/Rope2D/rope_segment.tscn")
 var body : RigidBody2D
 @export_node_path("Node2D") var segment_holder
 var body_joint: PinJoint2D
-var rope_sprite : Sprite2D
-const SPEED = 400.0
-const V_SPEED = 2.0
+
+const SPEED = 700.0
+const V_SPEED = 5.0
+var rope_seg: RigidBody2D 
+var rope_joint: PinJoint2D
+var rope_sprite: Sprite2D
+
 
 func _ready():
 	body = get_node(body_path)
 	body_joint = body.get_node("Joint")
-	build_rope(1)
+	build_rope()
 
 
 func change_rope_length(length_diff):
-	var node_b = body_joint.node_b
+	var body_node_b = body_joint.node_b
 	body_joint.node_b = ""
-	var rope_dir = (body_joint.global_position - global_position).normalized()
+	var rope_dir = (body_joint.global_position - rope_joint.global_position).normalized()
 	body.position += rope_dir * length_diff * V_SPEED
-	body_joint.node_b = node_b
-	update_rope_sprite(global_position.distance_to(body_joint.global_position))
+	var rope_node_b = rope_joint.node_b
+	rope_joint.node_b = ""
+	rope_seg.look_at(body_joint.global_position)
+	rope_seg.global_position = global_position
+	rope_joint.node_b = rope_node_b
+	body_joint.node_b = body_node_b
+	update_rope_sprite(rope_joint.global_position.distance_to(body_joint.global_position))
+
 
 func update_rope_sprite(new_length):
 	rope_sprite.scale.y = new_length / 64
 
-func build_rope(number_of_segments):
-	var current_seg: RigidBody2D = null
-	var next_seg_joint_pos = Vector2(0, 3400)
-	for i in range(number_of_segments):
-		
-		var next_seg: RigidBody2D = rope_seg_preload.instantiate()
-		rope_sprite = next_seg.get_node("Sprite2D")
-		update_rope_sprite(3400)
-		var next_joint: PinJoint2D = next_seg.get_node("Joint")
-		get_node(segment_holder).add_child(next_seg)
-		var next_pos = position
-		if (current_seg):
-			next_pos = current_seg.position + next_seg_joint_pos
-		next_seg.position = next_pos
-			
-		next_joint.node_b = self.get_path()
-		if (current_seg):
-			next_joint.node_b = current_seg.get_path()
-		print(next_seg)
-		current_seg = next_seg
-	var body_joint = body.get_node("Joint")
-	body.position = current_seg.position + next_seg_joint_pos - \
+
+func build_rope():
+	body.global_position.x = global_position.x
+	var rope_lenght = global_position.distance_to(body.global_position)
+
+	rope_seg = rope_seg_preload.instantiate()
+	rope_sprite = rope_seg.get_node("Sprite2D")
+	update_rope_sprite(rope_lenght)
+	rope_joint = rope_seg.get_node("Joint")
+	get_node(segment_holder).add_child(rope_seg)
+	rope_seg.position = position
+	rope_seg.look_at(body_joint.global_position)
+	rope_joint.node_b = self.get_path()
+	body.position = rope_seg.position + Vector2(0, rope_lenght) - \
 		body_joint.position
-	body_joint.node_b = current_seg.get_path()
+	body_joint.node_b = rope_seg.get_path()
 
 
 func _physics_process(delta):
